287ce0599042db64cc1b601fce5e85127f701cd9
Abbi Marmer-Adams <amarmerada@brynmawr.edu>
2022-08-31 20:46:03 -0400

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/************************************
+ * fortune.c
+ * Author: Abigail Marmer-Adams
+ * Date: 1/11/2022
+ * Change hello.c
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("All hail the glow cloud\n");
+    return 0;
+}
