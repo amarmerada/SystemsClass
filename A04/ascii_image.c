2ab45af2e3d921e35e53a1aed95e7696cc069339
Abbi Marmer-Adams <amarmerada@brynmawr.edu>
2022-10-18 21:33:41 -0400

    A04 complete

--- A04/ascii_image.c
+++ A04/ascii_image.c
@@ -1,11 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "read_ppm.h"
+
+
 
 int main(int argc, char** argv) {
   if (argc != 2) {
     printf("usage: ascii_image <file.ppm>\n");
     return 0;
+  }
+  int width;
+  int height;
+  FILE* fp;
+  fp = fopen(argv[1], "rb");
+  char buff[1024];
+  fgets(buff, 1024, fp);
+  if((buff[0]!= 'P') | (buff[1]!= '6')){
+    printf("incorrect file format (must be P6) \n");
+    exit(1);
+  }
+  printf("%c%c   \n", buff[0], buff[1]);
+  fgets(buff, 1024, fp);
+  printf("%s", buff);
+  fscanf(fp, "%d %d", &width, &height);
+  printf("%d %d\n", width, height);
+  fclose(fp);
+  int*x = &width;
+  int*y = &height;
+  char* nameOfFile = (char*)malloc(argc*sizeof(char*));
+  strcpy(nameOfFile, argv[1]);
+  struct ppm_pixel** asciiTarget = read_ppm_2d(nameOfFile, x ,y);
+  struct ppm_pixel hold;
+  unsigned char each = 0;
+  for (int i = 0; i < height; i++){
+    for (int j = 0; j < width; j++){
+      hold = asciiTarget[i][j];
+      each = hold.red + hold.blue + hold.green;
+      
+      if((each >= 0)&& (each < 26)){
+        printf("@");
+      }
+      else if((each >= 26)&&( each <51)){
+        printf("#");
+      }
+      else if((each >= 51)&&( each <76)){
+        printf("$");
+      }
+      else if((each >= 76)&&( each <101)){
+        printf("*");
+      }
+      else if((each >= 101)&&( each <126)){
+        printf("o");
+      }
+      else if((each >= 126)&&( each <151)){
+        printf(";");
+      }
+      else if((each >= 151)&&( each <176)){
+        printf(":");
+      }
+      else if((each >= 176)&&( each <201)){
+        printf(",");
+      }
+      else if((each >= 201)&&( each <226)){
+        printf(".");
+      }
+      else{
+        printf(" ");
+      }
+
+      if(j == width-1){
+        printf("\n");
+      }
+    }
   }
 
   return 0;
--- A04/read_ppm.c
+++ A04/read_ppm.c
@@ -13,43 +13,71 @@ struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
   return NULL;
 }*/
 
+void printpixels(struct ppm_pixel** toPrint, int height, int width){
+  struct ppm_pixel printer;
+  unsigned char red;
+  unsigned char green;
+  unsigned char blue;
+  for (int i = 0; i < height; i ++){
+    for (int j = 0; j < width; j++){
+      printer = toPrint[i][j]; 
+      red = printer.red;
+      green = printer.green;
+      blue = printer.blue;
+      if(j == width -1){
+        printf("( %u, %u, %u ) \n", red, green, blue);
+      }
+      else{
+        printf("( %u, %u, %u ) ", red, green, blue);
+      }
+    }
+  }
+
+}
+
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
-  int weight = *w;
+  int width = *w;
   int height = *h;
   FILE* fp;
-  fp = fopen("feep-raw.ppm", "rb");
-  struct ppm_pixel** rtn = (struct ppm_pixel**)malloc(weight*height*sizeof(struct ppm_pixel));
+  fp = fopen(filename, "rb");
   
-  char buff[20];
-  fgets(buff, 20, fp);
+  char buff[1024];
+  fgets(buff, 1024, fp);
   if((buff[0]!= 'P') | (buff[1]!= '6')){
     printf("incorrect file format (must be P6) \n");
     exit(1);
   }
   printf("%c%c   \n", buff[0], buff[1]);
-  fgets(buff, 20, fp);
+  fgets(buff, 1024, fp);
   printf("%s", buff);
-  fgets(buff, 20, fp);
+  fscanf(fp, "%d %d", &width, &height);
+  printf("%d %d\n", width, height);
+  fgets(buff, 1024, fp);
+  //printf("%s", buff);
+  fgets(buff, 1024, fp);
   printf("%s", buff);
-  fgets(buff, 20, fp);
-  printf("%s", buff);
-  fgets(buff, 20, fp);
-  printf("%s", buff);
-  fgets(buff, 20, fp);
-  printf("%s", buff);
-  unsigned int x;
-  fgetu(x, fp);
-  printf("%u", x);
+  
+  struct ppm_pixel** rtn = (struct ppm_pixel**)malloc(height*sizeof(struct ppm_pixel));
+  for (int i = 0; i < height; i++){
+    rtn[i] = (struct ppm_pixel*)malloc(width*sizeof(struct ppm_pixel));
+    fread(rtn[i], sizeof(struct ppm_pixel), width, fp);
+  }
   fclose(fp);
+  //printpixels(rtn, height, width);
+  *w = width;
+  *h = height;
+  
   return rtn;
 }
 
-int main(){
-  int a = 5;
-  int b = 6;
+/**int main(int argc, char** argv){
+  char* nameOfFile = (char*)malloc(argc*sizeof(char*));
+  strcpy(nameOfFile, argv[1]);
+  int a = 200;
+  int b = 300;
   int*x = &a;
   int*y = &b;
-  read_ppm_2d("feep-raw.ppm", x, y);
+  read_ppm_2d(nameOfFile, x, y);
 return 0;
-}
+}*/
 
